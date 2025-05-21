81c7c0669d6d89c3fa40cd69420a84eade8ba491
Mouriya <mouriyavk.official@gmail.com>
2025-05-21 15:56:40 +0530

    it can go through all the files

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -5,8 +5,18 @@ return {
   {
     "nvim-telescope/telescope.nvim",
     tag = "0.1.5",
-    dependencies = { "nvim-lua/plenary.nvim" },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "ahmedkhalf/project.nvim", -- Added project.nvim as dependency
+    },
     config = function()
+      -- Set up project.nvim
+      require("project_nvim").setup({
+        detection_methods = { "pattern", "lsp" },
+        patterns = { ".git", "package.json", "Makefile", "README.md" },
+        show_hidden = true,
+      })
+
       require("telescope").setup({
         extensions = {
           ["ui-select"] = {
@@ -14,11 +24,17 @@ return {
           },
         },
       })
+
+      -- Load Telescope extensions
+      require("telescope").load_extension("ui-select")
+      require("telescope").load_extension("projects")
+
+      -- Keymaps
       local builtin = require("telescope.builtin")
       vim.keymap.set("n", "<C-p>", builtin.find_files, {})
       vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-
-      require("telescope").load_extension("ui-select")
+      vim.keymap.set("n", "<leader>fp", "<cmd>Telescope projects<CR>", { noremap = true, silent = true })
     end,
   },
 }
+
